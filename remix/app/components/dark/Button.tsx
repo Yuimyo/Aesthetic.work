6d222101f41bd79cef6db264e9ca9ed985755e12
Yuimyo <Yuimyo@users.noreply.github.com>
2024-04-24 17:38:30 +0900

    fix: add props for Button

--- remix/app/components/dark/Button.tsx
+++ remix/app/components/dark/Button.tsx
@@ -1,10 +1,15 @@
 import type { LinksFunction } from "@remix-run/node";
 import styles from "./button.module.css";
 
-export default function Button() {
+interface ButtonProps {
+    children: React.ReactNode;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export default function Button({ children, onClick }: ButtonProps) {
     return (
         <div>
-            <button className={styles.btn}>永いエラーと奮闘の末</button>
+            <button className={styles.btn} onClick={onClick}>{children}</button>
         </div>
     );
 }
