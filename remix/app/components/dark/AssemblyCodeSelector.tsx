6682001df2e47876c81e2b0300ded46132bba02d
Yuimyo <Yuimyo@users.noreply.github.com>
2024-04-24 17:51:03 +0900

    fix: add props for AssemblyCodeSelector

--- remix/app/components/dark/AssemblyCodeSelector.tsx
+++ remix/app/components/dark/AssemblyCodeSelector.tsx
@@ -1,11 +1,35 @@
+import { ChangeEvent, useEffect, useState } from "react";
 import styles from "./AssemblyCodeSelector.module.css";
 
-export default function AssemblyCodeSelector() {
+interface AssemblyCodeSelectorProps {
+    onChange: (lineNumber: number) => void;
+    defaultValue: number;
+    data: string[];
+}
+
+export default function AssemblyCodeSelector({ onChange, defaultValue, data }: AssemblyCodeSelectorProps) {
+    let [selectedLine, setSelectedLine] = useState(defaultValue);
+
+    useEffect(() => {
+        console.log("changed selected line:", selectedLine); // TODO
+    }, [selectedLine]);
+
+
+    const handleButtonClick = (lineNumber: number) => {
+        setSelectedLine(prev => lineNumber);
+        onChange(lineNumber);
+    };
+
     return (
         <div>
             <div className={styles.viewer}>
                 <button className={styles.asm_line}>mov 2, rax</button>
                 <button className={styles.asm_line}>sub rax, 8</button>
+                {data.map((line, index) => (
+                    <button key={index} onClick={() => handleButtonClick(index + 1)}>
+                        {line}
+                    </button>
+                ))}
             </div>
         </div>
     );
