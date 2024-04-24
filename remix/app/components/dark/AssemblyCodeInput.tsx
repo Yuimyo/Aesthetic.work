ac16c1383e3c4b4f1588fde08a2a7d5330945bd0
Yuimyo <Yuimyo@users.noreply.github.com>
2024-04-24 17:37:46 +0900

    fix: add props for AssemblyCodeInput

--- remix/app/components/dark/AssemblyCodeInput.tsx
+++ remix/app/components/dark/AssemblyCodeInput.tsx
@@ -1,9 +1,14 @@
+import { ChangeEvent } from 'react';
 import styles from "./AssemblyCodeInput.module.css";
 
-export default function AssemblyCodeInput() {
+interface AssemblyCodeInputProps {
+    onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export default function AssemblyCodeInput({ onChange }: AssemblyCodeInputProps) {
     return (
         <div>
-            <textarea className={styles.asm_form} id="code-input" placeholder="addq $0x2A %rax" spellCheck="false"></textarea>
+            <textarea className={styles.asm_form} onChange={onChange} id="code-input" placeholder="addq $0x2A %rax" spellCheck="false"></textarea>
         </div>
     );
 }
